2ad825238b701aa125638b2f438f5b5df85eb143
DhimasBagusB <103429148+DhimasBagusB@users.noreply.github.com>
2022-06-18 13:44:15 +0700

    Add files via upload

---
+++ Tugas8/src/Healer.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.Math;
+public class Healer extends Character{
+
+    public double random(){
+        double random_a = 0;
+        double random_b = 10;
+        double random = Math.floor(Math.random()*(random_b-random_a+1)+random_a);
+        return  random;
+    }
+
+    public boolean attack(){
+        if (random() > 0 && random() <= 8.5){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void heal(){
+        setHP(getHP() + 25);
+    }
+}
---
+++ Tugas8/src/Titan.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Titan extends Character{
+    public double random(){
+        double random_a = 0;
+        double random_b = 10;
+        double random = Math.floor(Math.random()*(random_b-random_a+1)+random_a);
+        return  random;
+    }
+
+    public boolean attack(){
+        if (random() > 0 && random() <= 4){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
